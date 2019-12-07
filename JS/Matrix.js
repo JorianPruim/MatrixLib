aec2426a731f93c9949516ec869934e905b82fdf
JorianPruim <jorian.pruim@gmail.com>
2019-12-07 11:40:55 +0100

    Update Matrix.js
    matrices can hold values

--- JS/Matrix.js
+++ JS/Matrix.js
@@ -10,6 +10,7 @@ function Matrix(x,y){
 		}
 		this.dimX += n;
 	}
+
 	this.addRows = function(n){//->void
 		for(var i=0;i<n;i++){
 			this.val[this.dimY+i] = new Array();
@@ -20,19 +21,37 @@ function Matrix(x,y){
 		this.dimY+=n;
 	}
 
+	this.altRow = function(y,arr){//array->void
+		if(typeof(arr)!="object" || arr.length!=this.dimX){
+			console.warn("Warning: faulty array recieved, expected array of length " + this.dimX);
+		}else if(y>this.dimY || y<0){
+			console.warn("Warning: row does not exist");
+		}else{
+			this.val[y] = arr;
+		}
+	}
 
-
-
+	this.altColumn = function(x,arr){
+		if(typeof(arr)!="object" || arr.length!=this.dimY){
+			console.warn("Warning: faulty array recieved, expected array of length " + this.dimY);
+		}else if(x>this.dimX || x<0){
+			console.warn("Warning: column does not exist");
+		}else{
+			for(var i=0;i<this.dimY;i++){
+				this.val[i][x] = arr[i];
+			}
+		}
+	}
 
 	for(var i=0;i<y;i++){
 		this.val[i] = new Array();
 		for(var j=0;j<x;j++){
 			this.val[i][j] = 0;
-		}
+		}//init
 	}
 }
 /*Todo
-alter rows/columns
+
 drop rows/columns
 get rows/columns
 matrix addition
